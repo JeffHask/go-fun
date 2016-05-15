41397c3cb7fb073fac554e5f443ac5774c25846a
Jeffrey Haskovec <jeffhaskovec@gmail.com>
2016-05-15 18:40:07 -0500

    Removed the hello world, now starting on the file system traversing

--- hello.go
+++ hello.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	//"github.com/JeffHask/stringutil"
+	"path/filepath"
+	//"flag"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	//fmt.Printf("Hello, world.\n")
+	error := os.Chdir("..")
+	if(error != nil) {
+		fmt.Printf("blarf, error", error)
+		return
+	}
+	s := os.Args[0]
+	for i := 0; s != "C:\\"; i++ {
+		fmt.Printf("%d\n", i)
+		fmt.Printf(s + "\n")
+		s = filepath.Dir(s)
+	}
+	fmt.Printf(s)
+	// os.Chdir("..")
+	//os.Mkdir("blahz", os.ModeDir)
+	//traverse(s)
+	//fmt.Printf(stringutil.Reverse("!oG, olleH\n"))
+}
+
+func traverse(s string) {
+	filepath.Walk(s, visit)
+}
+	
+func visit(path string, f os.FileInfo, err error) error {
+	fmt.Printf("Visited: %s\n", path)
+	return nil
 }
