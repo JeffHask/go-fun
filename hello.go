a7dec09a1fdcf8d159dc055a1cedecb6b84f1f2c
Jeffrey Haskovec <jeffhaskovec@gmail.com>
2016-05-20 13:07:49 -0500

    Updated to now show stats based on specific fields

--- hello.go
+++ hello.go
@@ -3,36 +3,66 @@ package main
 import (
 	"fmt"
 	"os"
-	//"github.com/JeffHask/stringutil"
+	"strings"
 	"path/filepath"
-	//"flag"
+	"go-id3-master/src/id3"
 )
 
+var fileMap map[string]int
+
+var count = 0
+
 func main() {
-	//fmt.Printf("Hello, world.\n")
+	fileMap = make(map[string]int)
 	error := os.Chdir("..")
 	if(error != nil) {
-		fmt.Printf("blarf, error", error)
+		fmt.Printf("%s", error)
 		return
 	}
-	s := os.Args[0]
-	for i := 0; s != "C:\\"; i++ {
-		fmt.Printf("%d\n", i)
-		fmt.Printf(s + "\n")
-		s = filepath.Dir(s)
-	}
-	fmt.Printf(s)
-	// os.Chdir("..")
-	//os.Mkdir("blahz", os.ModeDir)
-	//traverse(s)
-	//fmt.Printf(stringutil.Reverse("!oG, olleH\n"))
+	s := os.Args[1]
+	traverse(s)
+	fmt.Println(fileMap)
 }
 
 func traverse(s string) {
 	filepath.Walk(s, visit)
+	fmt.Printf("%d Songs\n", count)
+	for i, x := range fileMap {
+		var percent float64 = float64(x) / float64(count)
+		fmt.Printf("%d Songs in %s (%.3f%%)\n", x, i, percent * float64(100))
+	}
 }
-	
+
 func visit(path string, f os.FileInfo, err error) error {
-	fmt.Printf("Visited: %s\n", path)
+	if strings.Contains(filepath.Base(path),".mp3") {
+		fd, _ := os.Open(path)
+		defer fd.Close()
+		file := id3.Read(fd)
+		if file != nil {
+			count++
+			v := ""
+			if len(os.Args) > 2 {
+				v = os.Args[2]
+			}
+			switch v {
+			case "Album" :
+				v = file.Album
+				fileMap[file.Album] = fileMap[file.Album] + 1
+			case "Artist":
+				v = file.Artist
+				fileMap[file.Artist] = fileMap[file.Artist] + 1
+			case "Genre" :
+				v = file.Genre
+				fileMap[file.Genre] = fileMap[file.Genre] + 1
+			case "Year" :
+				v = file.Year
+				fileMap[file.Year] = fileMap[file.Year] + 1
+			default:
+				//TODO figure out default case
+				v = "REKT"
+			}
+			fmt.Println(v)
+		}
+	}
 	return nil
 }
